f6b676d20ca8215eb4413a85173ad1b384e4ff5b
jtjun88 <jun2jansen@gmail.com>
2024-11-01 23:50:47 +0800

    edits to mongo

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,14 +3,14 @@ import mongoose from "mongoose"
 const connectDB = async () => {
     try {
         
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(process.env.atlas_URI);
         // Mongoose to provide semi scheme to defining data model, (allowable fields and data types)
-        console.log('Connected to mongoDB')
+        console.log('Connected to mongoDB');
 
     } catch (error) {
-        console.error(`ERROR: ${error.message}`)
-        process.exit(1)
+        console.error(`ERROR: ${error.message}`);
+        process.exit(1);
     }
-}
+};
 
 export default connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,31 @@
 // import modules
 import path from 'path';
 import express from 'express';
-import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+dotenv.config();
 
 // import utils
 import connectDB from './config/database.js';
 
-// port listener
-dotenv.config();
+// port listener with hidden port variable in .env
+const port = process.env.PORT || 3000;
 
 // call database
 connectDB();
 
-// call function
+// express app
 const app = express();          
-app.use(express.json());        // set middleware to read json files in request
+
+// set middleware to read json files in request
+app.use(express.json());                            // parse json in request
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
+// route handler
 app.get("/", (req, res) => {
     res.send('Hello World')
 });
 
 // port listener
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on PORT: ${port}`));
-
+app.listen(port, () => console.log(`Server is running on PORT: ${port}`));
